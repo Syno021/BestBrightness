66c29251d59ab294d20fe18cd0be8af021848892
syno021 <synoivfour@gmail.com>
2024-09-15 19:37:10 +0200

    admin dashboard

--- src/app/admin-dashboard/admin-dashboard.page.ts
+++ src/app/admin-dashboard/admin-dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardPage implements OnInit {
 
-  constructor() { }
+  totalUsers: number = 0;
+  salesThisMonth: number = 0;
+  pendingOrders: number = 0;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.fetchUserCount();
+    this.fetchSalesCount();
+    this.fetchPendingOrdersCount();
   }
 
+  fetchUserCount() {
+    this.http.get<{ user_count: number }>('http://localhost/user_api/register.php')
+      .subscribe(response => {
+        this.totalUsers = response.user_count;
+      });
+  }
+
+  fetchSalesCount() {
+    this.http.get<{ sales_count: number }>('http://localhost/user_api/sales.php')
+      .subscribe(response => {
+        this.salesThisMonth = response.sales_count;
+      });
+  }
+
+  fetchPendingOrdersCount() {
+    this.http.get<{ pending_orders_count: number }>('http://localhost/user_api/orders.php')
+      .subscribe(response => {
+        this.pendingOrders = response.pending_orders_count;
+      });
+  }
 }
