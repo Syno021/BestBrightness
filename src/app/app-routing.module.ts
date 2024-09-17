322e42041fa5538a2a7e46e85917c5b72e771319
kego28 <marashapatience8@gmail.com>
2024-09-17 10:01:52 +0200

    products and cart patience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   {
@@ -58,7 +59,8 @@ const routes: Routes = [
   {
     path: 'account',
     loadChildren: () => import('./account/account.module').then(m => m.AccountPageModule)
-  },  {
+  },
+  {
     path: 'products',
     loadChildren: () => import('./products/products.module').then( m => m.ProductsPageModule)
   },
@@ -69,14 +71,17 @@ const routes: Routes = [
   {
     path: 'account',
     loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
-  }
-
+  },  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+  },
 
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    // HttpClientModule,
   ],
   exports: [RouterModule]
 })
--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController,IonSearchbar } from '@ionic/angular';
+import { CartService } from '../services/cart.service';
 
 interface Product {
   id: number;
   name: string;
   price: number;
   description: string;
+  image: string;
+  rating: number;
+  isSale?: boolean;
+  category: string;
+  
 }
 @Component({
   selector: 'app-products',
@@ -13,20 +21,82 @@ interface Product {
 })
 export class ProductsPage implements OnInit {
 
+  @ViewChild(IonSearchbar) searchbar!: IonSearchbar;
+
 products: Product[] = [
-    { id: 1, name: 'soap', price: 19.99, description: 'This is product 1' },
-    { id: 2, name: 'liquid', price: 29.99, description: 'This is product 2' },
-    { id: 3, name: 'cleaning attair', price: 39.99, description: 'This is product 3' },
+    { id: 1, name: 'soap', price: 19.99, description: 'This is product 1',image:'', rating:5, category:''},
+    { id: 2, name: 'liquid', price: 29.99, description: 'This is product 2',image:'', rating:5, category:'' },
+    { id: 3, name: 'liquid', price: 29.99, description: 'This is product 3',image:'', rating:5, category:'' },
+    { id: 4, name: 'cloth', price: 39.99, description: 'This is product 4',image:'' ,rating:4, category:'' },
+    { id: 5, name: 'gloves', price: 40.99, description: 'This is product 5',image:'' ,rating:3, category:'' },
+    { id: 6, name: 'mask', price: 90.99, description: 'This is product 6',image:'' ,rating:5, category:'' },
+    { id: 7, name: 'plastic bin', price: 60.99, description: 'This is product 7',image:'' ,rating:3, category:'' },
+    { id: 8, name: 'bottle chemi', price: 55.99, description: 'This is product 8',image:'' ,rating:4, category:'' },
+    { id: 9, name: 'pressure cleaner', price: 50.99, description: 'This is product 9',image:'' ,rating:5, category:'' },
   ];
 
-  constructor() { }
+  filteredProducts: Product[] = this.products;
+  categories: string[] = ['All', ...new Set(this.products.map(product => product.category))];
+  selectedCategory: string = 'All';
+  sortOption: string = 'name';
+
+
+  constructor(private cartservice: CartService, private toastController: ToastController) { }
 
   ngOnInit() {
+    this.applyFilters();
   }
 
-  addToCart(product: Product) {
-    console.log(`Added ${product.name} to cart`);
-    // Implement actual cart functionality here
+  
+  // applyFilters() {
+  //   throw new Error('Method not implemented.');
+  // }
 
+  searchProducts() {
+    this.applyFilters()
+  }
+
+  filterByCategory(category: string) {
+    this.selectedCategory = category;
+    this.applyFilters();
+  }
+
+  sortProducts(option: string) {
+    this.sortOption = option;
+    this.applyFilters();
+  }
+
+ applyFilters() {
+    const searchTerm = this.searchbar ? this.searchbar.value?.toLowerCase()?? '' : '';
+    
+    this.filteredProducts = this.products.filter(product => 
+      (this.selectedCategory === 'All' || product.category === this.selectedCategory) &&
+      product.name.toLowerCase().includes(searchTerm)
+    );
+
+switch(this.sortOption) {
+  case 'name':
+    this.filteredProducts.sort((a, b) => a.name.localeCompare(b.name));
+    break;
+  case 'price_low_high':
+    this.filteredProducts.sort((a, b) => a.price - b.price);
+    break;
+  case 'price_high_low':
+    this.filteredProducts.sort((a, b) => b.price - a.price);
+    break;
+  case 'rating':
+    this.filteredProducts.sort((a, b) => b.rating - a.rating);
+    break;
+}
+ }
+async addToCart(product: any) {
+  this.cartservice.addToCart(product);
+
+  const toast = await this.toastController.create({
+    message: `${product.name} added to cart`,
+    duration: 2000,
+    position: 'bottom'
+  });
+  toast.present();
 }
 }
