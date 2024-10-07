272899e784567bb2152dc1ddcbf8e8593be9c82b
kego28 <marashapatience8@gmail.com>
2024-10-07 16:14:54 +0200

    interface of signup patience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy, ModalController } from '@ionic/angular';
-import { MenuController } from '@ionic/angular';
+import { MenuController} from '@ionic/angular';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+
+// import { AuthFormComponent } from './auth-form/auth-form.component';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,21 +19,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { CategoryManagementComponent } from './category-management/category-management.component';
 
 @NgModule({
+  
   declarations: [AppComponent, NavbarComponent, CategoryManagementComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     AngularFireStorageModule,
+    AppRoutingModule,
     ReactiveFormsModule
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     MenuController,
     ModalController
+    
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
